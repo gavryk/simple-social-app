55b5f000c9c26425bcc27d98049a3414cdcd18a3
Gavryk <gvozdyk23@gmail.com>
2023-04-19 02:06:08 +0300

    server: create user api

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { getUser } from '../controllers/UserController.js';
+import { getUser, getUserFriends, updateFriends } from '../controllers/UserController.js';
 import { verifyToken } from '../middleware/index.js';
 
 const router = express.Router();
 
+//Read
 router.get('/:id', verifyToken, getUser);
+router.get('/:id/friends', verifyToken, getUserFriends);
+
+//Update
+router.patch('/:id/:friendId', verifyToken, updateFriends);
 
 export default router;

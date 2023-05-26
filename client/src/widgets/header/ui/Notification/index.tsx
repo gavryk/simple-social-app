4696b6002a620163ec0a587576256594713155bc
Gavryk <gvozdyk23@gmail.com>
2023-05-26 15:13:31 +0300

    cleint: nofication popup styles

--- client/src/widgets/header/ui/Notification/index.tsx
+++ client/src/widgets/header/ui/Notification/index.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import styles from './styles.module.scss';
 import clsx from 'clsx';
+import { UIButton } from '@/components';
 
 interface NotificationProps {
 	list?: string[];
 }
 
-export const Notifications: React.FC<NotificationProps> = ({ list = [] }) => {
-	return (
-		<div className={clsx(styles.root, { [styles.hasItems]: list.length > 0 })}>
-			{list.length > 0 ? (
-				<ul>
-					{list.map((item) => (
-						<li>{item}</li>
-					))}
-				</ul>
-			) : (
-				<div>
-					<span>No Notifications</span>
-				</div>
-			)}
-		</div>
-	);
+const clearNotification = () => {
+	console.log('Clear');
 };
+
+export const Notifications = React.forwardRef<HTMLDivElement, NotificationProps>(
+	({ list = [] }, ref) => {
+		return (
+			<div ref={ref} className={clsx(styles.root, { [styles.hasItems]: list.length > 0 })}>
+				{list.length > 0 ? (
+					<ul>
+						{list.map((item, index) => (
+							<li key={`${item}_${index}`}>{item}</li>
+						))}
+					</ul>
+				) : (
+					<div>
+						<span>No Notifications</span>
+					</div>
+				)}
+				<UIButton fluid color="green" size="xs" onClick={clearNotification}>
+					Clear
+				</UIButton>
+			</div>
+		);
+	},
+);

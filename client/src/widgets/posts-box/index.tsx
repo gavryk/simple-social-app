cf037b6c147247d45ce413d6758ca8f3c38cd6d5
gavryk <gvozdyk23@gmail.com>
2023-08-08 00:57:57 +0300

    client: post card (draft)

--- client/src/widgets/posts-box/index.tsx
+++ client/src/widgets/posts-box/index.tsx
@@ -1,9 +1,20 @@
 import { PostApiTypes } from '@/common/interfaces/postsApiTypes';
 import { UIBox } from '@/components';
 import React from 'react';
+import styles from './styles.module.scss';
 
 export const PostsBox: React.FC<PostApiTypes> = ({ posts }) => {
 	return (
-		<UIBox>{posts && posts.map((post) => <span key={post._id}>{post.description}</span>)}</UIBox>
+		<div className={styles.root}>
+			{posts &&
+				posts.map((post) => (
+					<UIBox key={post._id}>
+						<div className={styles.thumbnail}>
+							<img src={`${import.meta.env.VITE_BASE_URL}/uploads/${post.picturePath}`} alt="" />
+						</div>
+						<span>{post.description}</span>
+					</UIBox>
+				))}
+		</div>
 	);
 };
